969989bb5f9b27210be57f836fab8bf5b6f3399a
baku_nishiya <planet.of.early.summer.rain@gmail.com>
2024-06-13 21:57:56 +0900

    zustand-extraのお題実装コード追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        bg: "black",
-        color: "white",
-      },
-    },
-  },
-});
+import { ChakraProvider } from "@chakra-ui/react";
+import { useThemeStore } from "@/store/todo-extra";
+import { useEffect, useState } from "react";
+import theme from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { isDarkMode } = useThemeStore();
+  const [currentTheme, setCurrentTheme] = useState(theme(true));
+
+  useEffect(() => {
+    setCurrentTheme(theme(isDarkMode));
+  }, [isDarkMode]);
+
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={currentTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
